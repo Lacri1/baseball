7d70b289b415931189fbf2acf24277346c855fef
인경민 <wsy05076@gmail.com>
2025-09-03 15:09:41 +0900

    CrossOrigin 추가
    게시판쪽 CrossOrigin 추가

--- src/main/java/com/baseball/game/controller/BoardController.java
+++ src/main/java/com/baseball/game/controller/BoardController.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/board")
+@CrossOrigin(origins = { "http://localhost:3000" })
 public class BoardController {
 
 	private final BoardService boardService;
