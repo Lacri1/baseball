a11a494f532891589727ee20797615c8b4c7f8c8
인경민 <wsy05076@gmail.com>
2025-09-05 15:24:51 +0900

    게시판 댓글 수정
    게시판 댓글 등록 방법 수정

--- src/main/java/com/baseball/game/dto/CommentDto.java
+++ src/main/java/com/baseball/game/dto/CommentDto.java
@@ -1,10 +1,16 @@
 package com.baseball.game.dto;
+
 import lombok.Data;
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 @Data
 public class CommentDto {
-	private int commentId;
+	private Integer commentId;
 	private int boardNo;
 	private String writer;
 	private String text;
-	private String date;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime createdAt;
 }
